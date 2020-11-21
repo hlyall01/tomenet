a601b9586ead4a56d855104bfa17675ab9788099
Harry Lyall <hlyall@tofi.com.au>
2020-11-22 00:36:52 +1100

    actually fixed

--- init.sh
+++ init.sh
@@ -1,13 +1,8 @@
 cd /srv/build
-git clone https://github.com/Tokariew/tomenet.git tomenet
+git clone https://github.com/hlyall01/tomenet.git tomenet
 cd tomenet/src
-git remote add upstream https://github.com/TomenetGame/tomenet.git
-git pull upstream master
-git remote add upstream2 https://github.com/hlyall01/tomenet.git
-git pull upstream2 master
 git checkout mingw
 git merge --no-commit
-git pull upstream2 master --no-commit
 make -j 13 -f makefile.mingw tomenet.server.exe
 mingw-strip tomenet.server.exe
 mv tomenet.server.exe ..
